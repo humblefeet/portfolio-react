4100a0ccbf812f174e525985aca9b513ea70f62d
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-12-18 17:10:20 -0800

    fixed projects layout

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,30 +12,30 @@ const Projects = props =>(
 
         <div className="grid-container">
             <div className="module whatch">
-                <a  target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/ladystensberg/whatch">
+                <a      target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/ladystensberg/whatch">
                     <img className="images " src={whatch} alt="whatch screenshot"></img>
                 </a>
             </div>
             <div className="module hike">
                 <a  target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/humblefeet/hike-hitch">
+                        rel="noopener noreferrer"
+                        href="https://github.com/humblefeet/hike-hitch">
                     <img className="images " src={hikeHitch} alt="Hike Hitch screenshot"></img>
                 </a>
             </div>
             <div className="module rain">
-                <a  target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/humblefeet/rain-or-shine"> 
+                <a    target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/humblefeet/rain-or-shine"> 
                     <img className="images " src={rainOrShine} alt="Rain or Shine screenshot"></img>
                 </a>
             </div>
             <div className="module word">
-                <a  target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/humblefeet/Word-Scramble">
+                <a      target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/humblefeet/Word-Scramble">
                     <img className="images " src={unscrambler} alt="unscrambler screenshot"></img>
                 </a>
             </div>
